955fb604f36e1df8d01d0b21412008597eda3a32
Luka Vukovic <vukovicluka227@gmail.com>
2025-12-02 14:47:41 +0100

    day1 pt2

--- day1/program.js
+++ day1/program.js
@@ -10,7 +10,13 @@ function program() {
     const [direction, ...rest] = line;
     const steps = rest.join('');
     const clicks = Number(steps);
-    const mod = clicks % 100
+    const mod = clicks % 100;
+    const r = Math.floor(clicks / 100);
+    const holder = x
+    for (let i = 0; i < r; i++) {
+        zeroCounter++;
+    }
+
     if (direction === 'R') {
         x = x + mod
     } else {
@@ -19,8 +25,10 @@ function program() {
 
     if (x < 0) {
         x = 100 - Math.abs(x)
+        if (holder > 0 && x !== 0) zeroCounter++
     } else if (x > 99) {
         x = x - 100
+        if (holder < 100 && x !== 0) zeroCounter++
     }
 
     if (x === 0) {
